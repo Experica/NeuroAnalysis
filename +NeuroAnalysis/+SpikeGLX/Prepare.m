c7f2d6720d82332e72a4a4e8eea86063b5a4d79e
Li Alex Zhang <fff008@gmail.com>
2019-04-29 15:12:15 -0700

    check NTFS file name length for concat binary file

--- +NeuroAnalysis/+SpikeGLX/Prepare.m
+++ +NeuroAnalysis/+SpikeGLX/Prepare.m
@@ -49,21 +49,12 @@ if(exist(exfilepath,'file')==2)
 end
 
 isstimulatordata = false;
-
-% analyzerFile = dir(strcat(filedir, '\', '*.analyzer'));
-% 
-% if length(filename) > length(analyzerFile(1).name)
-%     analyzerName = filename(1:12);
-% else
-%     analyzerName = filename;
-% end
-
 analyzerfilepath =fullfile(filedir,[filename, '.analyzer']);
 if(exist(analyzerfilepath,'file')==2)
     isstimulatordata = true;
 end
 
-metafilenames=arrayfun(@(x)x.name,dir(fullfile(filedir,[filename,'*.meta'])),'uniformoutput',0);   % Find meta file from the name of analyzer file
+metafilenames=arrayfun(@(x)x.name,dir(fullfile(filedir,[filename,'*.meta'])),'uniformoutput',0); % Find meta file from the name of analyzer file
 if ~isempty(metafilenames)
     isspikeglxdata = true;
 else
